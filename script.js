3890e29db5b1427ca0986c203a0d7bab6384a042
John-Rep <replogle.john@next-u.fr>
2025-06-28 11:21:30 +0200

    API communication

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 let messageCount = 0;
 
 document.addEventListener("mousedown", function () {
-    console.log("Clicked");
+    message = "New Message: " + messageCount;
+    fetch("https://message-api-test.onrender.com/message", {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "content": message
+        })
+    });
     h2 = document.createElement("h2");
-    h2.appendChild(document.createTextNode("New Message: " + messageCount));
+    h2.appendChild(document.createTextNode(message));
     document.getElementById("messages").appendChild(h2);
     messageCount++;
 })
+
+setInterval(async () => {
+    data = await fetch("https://message-api-test.onrender.com/message");
+    messageText = await data.json();
+    const messages = document.getElementById("messages");
+    messages.innerHTML = "";
+    messageText.forEach(object => {
+        if (object != null) 
+        messages.innerHTML += `<h2>${object.content}</h2>`;
+    });
+}, 5000)
